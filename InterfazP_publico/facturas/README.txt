9e86463a9def7f00cac69e383357aba43c5afcb7
jose2002jimenez16castro-a11y <jose2002jimenez16castro@gmail.com>
2026-01-26 19:20:41 -0500

    Add files via upload

---
+++ InterfazP_publico/facturas/README.txt
@@ -0,0 +1,1 @@
+Carpeta de datos generados en ejecución. Se deja vacía para publicar el proyecto sin datos personales.
---
+++ InterfazP_publico/src/interfazp/InterfazP.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfazp;
+
+import Ventanas.Logn;
+
+/**
+ *
+ * @author 59399
+ */
+public class InterfazP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Logn lg=new Logn();
+lg.setVisible(true);
+lg.setLocationRelativeTo(null);
+
+    }
+    
+}
